5ec2724f7c147d8e2bd43c6edcc855eb5c8dc4fd
Tobias <WinkTobi@aol.de>
2017-02-27 11:29:09 +0100

    PUSH

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 def recursive(n):
-	if not isinstance(n, int):
-		raise AttributeError("Wrong argument")
+    if not isinstance(n, int):
+        raise AttributeError("Wrong argument")
 	n -= 1
-	print(n)
-	if n <= 0:
-		return
+    print(n)
+    if n <= 0:
+        return
 	recursive(n)
 
 recursive(10)
+
+# 1231312313121
